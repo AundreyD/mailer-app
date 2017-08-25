eb0d8bd8b70f6e31e5040f736dc43392c9d998df
FRIESE <jf944t@att.com>
2017-08-25 13:17:07 -0400

    IT IS ALIVE

--- frontend/src/auth.js
+++ frontend/src/auth.js
@@ -1,11 +1,18 @@
 import axios from "axios";
+import cookies from "cookies";
+axios.defaults.xsrfCookieName = 'csrftoken'
+axios.defaults.xsrfHeaderName = 'X-CSRFToken'
+
+let csrfToken = document.cookie.split("csrftoken=")[1]
+console.log(csrfToken)
 
 module.exports = {
     
     getToken: function (username, pass, callback) {
         axios.post('/api/obtain-auth-token/', {
                 username: username,
-                password: pass
+                password: pass,
+                headers: {"X-CSRFToken": csrfToken},
         }).then(function (response) {
             callback({authenticated: true, token: response.token})
         }).catch((error) => {
@@ -22,7 +29,7 @@ module.exports = {
         this.getToken(username, pass, (response) => {
             console.log(response)
             if (response.authenticated) {
-                localStorage.token = response.token
+                localStorage.token = csrfToken
                 if (callback) {
                     callback(true)
                 }
